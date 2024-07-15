c386f9ebb0c530d82b8c4b95828baa874a595767
SrivathsaH <srivathsaharish16@gmail.com>
2024-07-15 13:32:18 +0530

    first commit

---
+++ SBMS-03-Greet_API/src/test/java/com/srivath/Sbms03GreetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srivath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbms03GreetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
